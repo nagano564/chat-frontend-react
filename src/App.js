a4e371441e4d70994323b0148e84716ba33020d2
Kenny Nagano <nagano564@gmail.com>
2018-06-28 13:27:38 -0400

    frontend

--- src/App.js
+++ src/App.js
@@ -1,20 +1,93 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cable from 'actioncable';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentChatMessage: '',
+      chatLogs: []
+    };
+  }
+
+  componentWillMount() {
+    this.createSocket();
+  }
+
+  createSocket() {
+    let cable = Cable.createConsumer('ws://localhost:3001/cable');
+    this.chats = cable.subscriptions.create({
+      channel: 'ChatChannel'
+    }, {
+      connected: () => {},
+      received: (data) => {
+        let chatLogs = this.state.chatLogs;
+        chatLogs.push(data);
+        this.setState({ chatLogs: chatLogs });
+      },
+      create: function(chatContent) {
+        this.perform('create', {
+          content: chatContent
+        });
+      }
+    });
+  }
+
+  renderChatLog() {
+    return this.state.chatLogs.map((el) => {
+      return (
+        <li key={`chat_${el.id}`}>
+          <span className='chat-message'>{ el.content }</span>
+          <span className='chat-created-at'>{ el.created_at }</span>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <div className='stage'>
+          <h1>Chat</h1>
+          <ul className='chat-logs'>
+            { this.renderChatLog() }
+          </ul>
+          <input
+            onKeyPress={ (e) => this.handleChatInputKeyPress(e) }
+            value={ this.state.currentChatMessage }
+            onChange={ (e) => this.updateCurrentChatMessage(e) }
+            type='text'
+            placeholder='Enter your message...'
+            className='chat-input' />
+          <button
+            onClick={ (e) => this.handleSendEvent(e) }
+            className='send'>
+            Send
+          </button>
+        </div>
       </div>
     );
+  }
+
+  updateCurrentChatMessage(event) {
+    this.setState({
+      currentChatMessage: event.target.value
+    });
+  }
+
+  handleChatInputKeyPress(event) {
+    if(event.key === 'Enter') {
+      this.handleSendEvent(event);
+    }//end if
+  }
+
+  handleSendEvent(event) {
+    event.preventDefault();
+    this.chats.create(this.state.currentChatMessage);
+    this.setState({
+      currentChatMessage: ''
+    });
   }
 }
 
